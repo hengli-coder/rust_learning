0c9f20c1a11c54298220827312e7fc5944d5999d
hengli <liheng__V@163.com>
2021-10-21 21:31:31 +0800

    create project

--- rust_learning/src/main.rs
+++ rust_learning/src/main.rs
@@ -3,7 +3,5 @@ use std::fs::*;
 use redis::*;
 
 fn main() {
-    let cli = redis::Client::open("").unwrap();
-    let mut conn = cli.get_connection().unwrap();
-    let data : &str= conn.get("").unwrap();
+    println!("{1}--{0}",0,"heng");
 }
